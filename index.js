140bba0d9d3ce236cfd192b662ff64ec5e6d9089
zwyak <vlad.senyuk97@gmail.com>
2020-02-21 17:01:03 +0300

    Updated index

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ setTimeout(()=>{
   db.films.bulkCreate([
   { title: 'Собачья жизнь', rating: '8.100', year: 2010, budget: 63000560, gross: 105000100 },
   { title: '8-жизней', rating: '9.100', year: 2013, budget: 23000560, gross: 201000100 },
-  { title: 'Проклятие', rating: '4.014', year: 2000, budget: 10000, gross: 23000 }
+  { title: 'Проклятие', rating: '4.014', year: 2000, budget: 10000, gross: 23000 },
+  { title: 'Миссия невыполнима', rating: '6.014', year: 2005, budget: 100000, gross: 2300000 }
   ]).then(() => {
     return db.films.findAll({raw:true});
   }).then(films => {
     console.log(films);
+  })
+
+  db.films.findByPk(1).then((f) => {
+    f.createActor({
+      name: 'Иван Ургант',
+      birth: '1980-01-01',
+      liked: 100
+    }).then((a) =>{
+      console.log(a);
+    })
   })
 
   //Bulk update
