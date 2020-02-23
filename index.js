63e56023a8ddb6846414860cf4a78ffe4070cc49
vlad-senyuk-1997 <vladsenyuk1975@gmail.com>
2020-02-23 19:23:26 +0300

    Added bulk update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const config = require('./config.json');
 const db = require('./models')(Sequelize, config)
 
 setTimeout(()=>{
-  //Bulk create
+  //1 - Bulk create - films
   db.films.bulkCreate([
   { title: 'Собачья жизнь', rating: '8.100', year: 2010, budget: 63000560, gross: 105000100 },
   { title: '8-жизней', rating: '9.100', year: 2013, budget: 23000560, gross: 201000100 },
@@ -15,5 +15,123 @@ setTimeout(()=>{
     console.log(films);
   })
 
-  //Bulk update
+  //Bulk create - actors
+  db.actors.bulkCreate([
+  { name: 'Peter Griffin', birth: '1960-10-11', liked: 14689},
+  { name: 'Lois Griffin', birth: '1965-02-24', liked: 12152},
+  { name: 'Chris Griffin', birth: '1990-01-01', liked: 5600},
+  { name: 'Meg Griffin', birth: '1989-12-30', liked: 1},
+  ]).then(() => {
+    return db.actors.findAll({raw:true});
+  }).then(actors => {
+    console.log(actors);
+  })
+
+  //Create projects
+  //1
+  db.films.findByPk(1)
+  .then(film=>{
+    if(!film) return;
+
+    db.actors.findByPk(1)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+
+    db.actors.findByPk(2)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+
+    db.actors.findByPk(3)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+
+    db.actors.findByPk(4)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+  });
+  //2
+  db.films.findByPk(2)
+  .then(film=>{
+    if(!film) return;
+
+    db.actors.findByPk(1)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+
+    db.actors.findByPk(3)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+
+    db.actors.findByPk(4)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+  });
+  //3
+  db.films.findByPk(3)
+  .then(film=>{
+    if(!film) return;
+
+    db.actors.findByPk(1)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+
+    db.actors.findByPk(4)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+  });
+
+  //3
+  db.films.findByPk(4)
+  .then(film=>{
+    if(!film) return;
+
+    db.actors.findByPk(2)
+        .then(actor=>{
+            if(!actor) return;
+            film.addActor(actor);
+    });
+  });
 }, 3000);
+
+setTimeout(() =>{
+  //2 - Bulk update
+  db.sequelize.query("SELECT actorId, count(*) FROM `projects` group by actorId having count(*) = 3",
+                  { type: db.sequelize.QueryTypes.SELECT})
+  .then(actors => {
+    actors.forEach((item, i) => {
+      db.actors.update(
+        { liked: 10000 },
+        { where: { id: item.actorId }}
+      ).then(() =>{
+        console.log('Actor was updated!');
+      }).catch((err) =>{
+        console.error("Actor wasn't updated");
+      })
+    });
+  })
+  .then(() =>{
+    db.actors.findAll({raw:true})
+    .then(actors =>{
+      console.log(actors);
+    });
+  });
+
+}, 5000)
